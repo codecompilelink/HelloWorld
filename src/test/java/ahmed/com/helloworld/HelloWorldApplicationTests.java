403f8640f7d9a883a989990c4e9bdc3635d43be8
mahmed <opensource@mahmed.net>
2023-03-23 01:59:16 +0500

    initial

---
+++ src/test/java/ahmed/com/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,18 @@
+package ahmed.com.helloworld;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
